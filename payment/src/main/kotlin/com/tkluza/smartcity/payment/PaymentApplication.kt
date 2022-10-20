df4fdaac5961b7667b27928ad0dccce686e75849
Tomasz Kluza <tomasz.kluza@picard.de>
2022-10-21 01:42:25 +0200

    feature: Payment service - some basic domain structure

--- payment/src/main/kotlin/com/tkluza/smartcity/payment/PaymentApplication.kt
+++ payment/src/main/kotlin/com/tkluza/smartcity/payment/PaymentApplication.kt
@@ -1,12 +1,12 @@
 package com.tkluza.smartcity.payment
 
-import com.tkluza.smartcity.payment.plugins.configureRouting
+import com.tkluza.smartcity.payment.business.routing.configurePaymentRouting
 import com.tkluza.smartcity.payment.plugins.configureSerialization
 import io.ktor.server.application.Application
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    configureRouting()
+    configurePaymentRouting()
     configureSerialization()
 }
---
+++ payment/src/main/kotlin/com/tkluza/smartcity/payment/business/routing/PaymentRouting.kt
@@ -0,0 +1,16 @@
+package com.tkluza.smartcity.payment.business.routing
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.configurePaymentRouting() {
+
+    routing {
+        get("/payments") {
+            call.respondText("Hello World!")
+        }
+    }
+}
