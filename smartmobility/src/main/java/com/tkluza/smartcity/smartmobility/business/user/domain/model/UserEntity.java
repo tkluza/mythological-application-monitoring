38292be67a273f78e87e19bde9adfe5b94860909
Tomasz Kluza <tomasz.kluza@picard.de>
2022-10-22 15:40:17 +0200

    feature: Smart mobility: User data model

--- smartmobility/src/main/java/com/tkluza/smartcity/smartmobility/business/user/domain/model/UserEntity.java
+++ smartmobility/src/main/java/com/tkluza/smartcity/smartmobility/business/user/domain/model/UserEntity.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -24,4 +26,14 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
 }
--- smartmobility/src/main/resources/data.sql
+++ smartmobility/src/main/resources/data.sql
@@ -1,4 +1,11 @@
+-- insert autonomous car data
 INSERT INTO t_autonomous_car (model, manufacturer, number_of_seats, registration_plate, battery_level)
 VALUES ('Z1', 'AUDI', 4, 'DWA-0001', 1),
        ('5000', 'FIAT', 2, 'KRX-0002', 0.5),
        ('20', 'TESLA', 7, 'SKY-0001', 0.2);
+
+-- insert user data
+INSERT INTO t_user(first_name, last_name, email)
+VALUES ('Michael', 'Jordan', 'michael.jordan@chicago-bulls.com'),
+       ('Magic', 'Johnson', 'magic.johnson@los-angeles-lakers.com'),
+       ('Dirk', 'Nowitzki', 'dirk.nowitzki@dallas-mavericks.com');
